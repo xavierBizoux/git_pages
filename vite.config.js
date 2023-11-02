728b72cca869282b28341477d715a78dfa05781d
Xavier Bizoux <xavier.bizoux@sas.com>
2023-11-02 10:15:05 +0100

    Change base variable

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/git_pages",
   server: {
     host: "localhost",
     port: 3000
