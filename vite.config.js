2bf06abc55b9b766e4ce9736b6dcf05e482ee82e
Xavier Bizoux <xavier.bizoux@sas.com>
2023-11-02 10:18:17 +0100

    Change base parameter

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/git_pages",
+  base: "/git_pages/",
   server: {
     host: "localhost",
     port: 3000
